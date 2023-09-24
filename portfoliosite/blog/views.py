c15d09dea5217bc72bf43105aa2cdbc33418ad11
arhsimluhar <rahul.mishra2003@gmail.com>
2023-09-24 21:51:10 +0530

    adding canonical URL for Post

--- portfoliosite/blog/views.py
+++ portfoliosite/blog/views.py
@@ -10,7 +10,8 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, published__year=year,
+                             published__month=month, published__day=day,
+                             status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
-
